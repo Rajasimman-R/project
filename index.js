1967917112bdea5bebd7ed66527a8551a8014cb3
Rajasimman-R <131901523+Rajasimman-R@users.noreply.github.com>
2023-09-30 22:02:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+section {
+    margin-bottom:
+  [30-09-2023 21:58] V P Manoj kumar Accio Jobs: document.addEventListener("DOMContentLoaded", function() {
+    var heroImage = document.querySelector(".hero-text img");
+  
+    heroImage.classList.add("loaded");
+  });
